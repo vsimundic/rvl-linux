2976cbcfaed12de85b13c4f42b7e4437e4a85d82
dfilko <dfilko@etfos.hr>
2017-04-18 13:07:49 +0200

    Added new options for MultilateralSmoothMesh which adjusts filter's behaviour for boundary points: Boundary1DFiltering which uses first and second level neighbour points along the boundary, onlyFirstNeigh_1DFiltering (used with Boundary1DFiltering) use only first level neighbour points along the boundary and current (central) poimt.

--- RVLObjectDetection/ObjectDetector.cpp
+++ RVLObjectDetection/ObjectDetector.cpp
@@ -263,7 +263,7 @@ vtkSmartPointer<vtkPolyData> MultilateralSmoothMesh(vtkSmartPointer<vtkPolyData>
 
 //Dirk Holz and Sven Behnke: "Approximate Triangulation and Region Growing for Efficient Segmentation and Smoothing of Range Images"
 //NOT DEBUGGED
-void MultilateralSmoothMesh(Mesh *pMesh, int noIter, bool verbose = false)
+void MultilateralSmoothMesh(Mesh *pMesh, int noIter, bool Boundary1DFiltering, bool onlyFirstNeigh_1DFiltering = false, bool verbose = false)
 {
 	//get a copy of pPolygonData points (destination points for first iteration)
 	vtkSmartPointer<vtkPoints> pointsSource = pMesh->pPolygonData->GetPoints();
@@ -284,11 +284,13 @@ void MultilateralSmoothMesh(Mesh *pMesh, int noIter, bool verbose = false)
 	double nextEdgePointD[3];
 	MeshEdge* pEdge;
 	MeshEdgePtr* pEdgePtr;
+	MeshEdge* pEdge2;
+	MeshEdgePtr* pEdgePtr2;
 	int othersideIdx;
+	int otherothersideIdx;
 	int noPointEdges = 0;
 	int neighbourPoints[20]; //assumption: there is maximum 20 edges for any point
 	bool boundaryEdges[20];
-
 
 	float tempDist2 = 0.0;
 	float tempL1 = 0.0;
@@ -302,40 +304,103 @@ void MultilateralSmoothMesh(Mesh *pMesh, int noIter, bool verbose = false)
 	float sumW = 0.0;
 	float sumPts[3];
 	float sumNorm[3];
-
+	bool first = true;
+	bool found = false;
 	for (int iter = 0; iter < noIter; iter++)
 	{
 		for (int idx = 0; idx < pMesh->NodeArray.n; idx++)
 		{
 			//current point and edge list
 			currPoint = pMesh->NodeArray.Element + idx;
-			//if ((currPoint->P[0] == 0.0) && (currPoint->P[1] == 0.0) && (currPoint->P[2] == 0.0))
-			//	continue;
 			if (!currPoint->bValid)
 				continue;
 			pointsSource->GetPoint(idx, currPointD);
 			normalsSource->GetTupleValue(idx, currNor);
 			pEdgePtr = currPoint->EdgeList.pFirst;
 			noPointEdges = 0;
-			while (pEdgePtr)
+			if (Boundary1DFiltering && currPoint->bBoundary)//If the point is on the boundary and we use 1D boundary filtering
 			{
-				RVLPCSEGMENT_GRAPH_GET_NEIGHBOR(idx, pEdgePtr, pEdge, othersideIdx);	//get the other side index and point
-				nextEdgePoint = pMesh->NodeArray.Element + othersideIdx;
-				neighbourPoints[noPointEdges] = othersideIdx;	//set other side index as in the neighbourhood
-				if ((noPointEdges == 0) || (pEdgePtr->pNext == NULL))	//Check if that edge is boundary
-					boundaryEdges[noPointEdges] = true;
-				else
-					boundaryEdges[noPointEdges] = false;
+				while (pEdgePtr)
+				{
+					RVLPCSEGMENT_GRAPH_GET_NEIGHBOR(idx, pEdgePtr, pEdge, othersideIdx);	//get the other side index and point
+					nextEdgePoint = pMesh->NodeArray.Element + othersideIdx;
+					if ((noPointEdges == 0) || (pEdgePtr->pNext == NULL))	//boundary is only first and last pointer?
+					{
+						neighbourPoints[noPointEdges] = othersideIdx;
+						boundaryEdges[noPointEdges] = true;
+						noPointEdges++;
+						if (!onlyFirstNeigh_1DFiltering)	//if we use first and second neighbours
+						{
+							//find neighbours boundary edges
+							pEdgePtr2 = nextEdgePoint->EdgeList.pFirst;
+							first = true;
+							while (pEdgePtr2)
+							{
+								RVLPCSEGMENT_GRAPH_GET_NEIGHBOR(othersideIdx, pEdgePtr2, pEdge2, otherothersideIdx);	//get the other side index
+								if ((first || (pEdgePtr2->pNext == NULL)) && (otherothersideIdx != idx)) //boundary is only first and last pointer? //Also it must not be the poiter to current point
+								{
+									//Check if it is not already on the list
+									found = false;
+									for (int i = 0; i < noPointEdges; i++)
+									{
+										if (neighbourPoints[i] == otherothersideIdx)	//If it is on the list
+										{
+											found = true;
+											break;
+										}
+									}
+									if (!found)	//If it is not already in, add it
+									{
+										neighbourPoints[noPointEdges] = otherothersideIdx;
+										boundaryEdges[noPointEdges] = true;
+										noPointEdges++;
+									}
+								}
+								first = false;	//It is no longer the first pointer
+								pEdgePtr2 = pEdgePtr2->pNext;
+							}
+						}
+					}
 
-				noPointEdges++;
-				pEdgePtr = pEdgePtr->pNext;
+					pEdgePtr = pEdgePtr->pNext;
+				}
+			}
+			else   //Standard multilateral filter takes all neighbours into considiration regardless of boundary
+			{
+				while (pEdgePtr)
+				{
+					RVLPCSEGMENT_GRAPH_GET_NEIGHBOR(idx, pEdgePtr, pEdge, othersideIdx);	//get the other side index and point
+					nextEdgePoint = pMesh->NodeArray.Element + othersideIdx;
+					neighbourPoints[noPointEdges] = othersideIdx;	//set other side index as in the neighbourhood
+					if ((noPointEdges == 0) || (pEdgePtr->pNext == NULL))	//Check if that edge is boundary //first and last are boundary edges
+						boundaryEdges[noPointEdges] = true;
+					else
+						boundaryEdges[noPointEdges] = false;
+
+					noPointEdges++;
+					pEdgePtr = pEdgePtr->pNext;
+				}
 			}
 
 			sumPts[0] = 0.0; sumPts[1] = 0.0; sumPts[2] = 0.0;
 			sumNorm[0] = 0.0; sumNorm[1] = 0.0; sumNorm[2] = 0.0;
 			sumW = 0.0;
-			for (int id_curr = 0; id_curr < noPointEdges; id_curr++)
+
+			if (Boundary1DFiltering && onlyFirstNeigh_1DFiltering && currPoint->bBoundary)	//If we take only first neighbours into considiration for 1D boundary filtering we also use the current (central point)
 			{
+				sumW += 1.0;
+				sumPts[0] += currPointD[0];
+				sumPts[1] += currPointD[1];
+				sumPts[2] += currPointD[2];
+				sumNorm[0] += currNor[0];
+				sumNorm[1] += currNor[1];
+				sumNorm[2] += currNor[2];
+			}
+
+			for (int id_curr = 0; id_curr < noPointEdges; id_curr++)	//for each neighbour on the list
+			{
+				/*if (boundaryEdgeFiltering && currPoint->bBoundary && !boundaryEdges[id_curr])
+					continue;*/
 				pointsSource->GetPoint(neighbourPoints[id_curr], otherPt);
 				normalsSource->GetTupleValue(neighbourPoints[id_curr], otherNor);
 				//calculate distance
@@ -401,6 +466,8 @@ void MultilateralSmoothMesh(Mesh *pMesh, int noIter, bool verbose = false)
 		currPoint->P[1] = currPointD[1];
 		currPoint->P[2] = currPointD[2];
 	}
+	if (verbose)
+		std::cout << "Mesh data updated!" << std::endl;
 }
 
 void LaplaceSmooting(Mesh *pMesh, int noIter, bool useCotan)
@@ -695,7 +762,7 @@ void ObjectDetector::DetectObjects(char *MeshFilePathName)
 
 		//SmoothMesh(&mesh, 30);
 		//LaplaceSmooting(&mesh, 30);
-		MultilateralSmoothMesh(&mesh, 10, true);
+		MultilateralSmoothMesh(&mesh, 10, false, false, true);
 
 		// Segment mesh to surfels.				
 
